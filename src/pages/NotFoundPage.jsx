dfe3c28e70b18fed32b672262f31b64cc7b97fd9
Mia <mgeoffra@purdue.edu>
2025-05-01 15:08:44 -0400

    NotFoundPage fix

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const NotFoundPage = () => {
   return (
     <div>
-      <h1>404</h1>
+      <h1 style={{textAlign:"center"}}>404</h1>
       <p style={{textAlign:"center"}}>Sorry, the page you are looking for is not found.</p>
       <Link to="/" style={{textAlign:"center", display: "block"}}>Go back to Home</Link>
     </div>
